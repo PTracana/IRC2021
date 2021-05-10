49d25b555c2d3a88ae4250572a7b4062911f11c5
Pedro <43250725+PTracana@users.noreply.github.com>
2021-05-10 18:31:15 +0100

    Update tcp-server.py

--- tcp-server.py
+++ tcp-server.py
@@ -21,11 +21,17 @@ def handle_client_connection(client_socket):
     client_socket.send(msg_to_client)
     client_socket.close()
 
-while True:
-    client_sock, address = server.accept()
-    print ('Accepted connection from {}:{}'.format(address[0], address[1]))
-    client_handler = threading.Thread(
-        target=handle_client_connection,
-        args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
-    )
-    client_handler.start()
+
+def main():
+    global server
+    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((bind_address, bind_port))
+    server.listen(100);
+    print("Listening on {}:{}".format(bind_address, bind_port))
+    signal.signal(signal.SIGINT, exit_server)
+    while  True:
+        client_sock, address = server.accept()
+        print("Acepted connection from {}:{}".format(address[0],address[1]))
+        client_handler=threading.Thread(target=handle_client_connection, args=(client_sock, ))
+        client_handler.start()
+
