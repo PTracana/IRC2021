b03ed2682565fd647d3fb2355ca9833543ff074b
Pedro <43250725+PTracana@users.noreply.github.com>
2021-05-23 20:49:02 +0100

    works now
    improved the server to actually have basic functionality and close properly

--- tcp-client.py
+++ tcp-client.py
@@ -1,65 +1,44 @@
 import socket, signal, sys, select, os
 
-server_ip = [socket.gethostbyname("pedro.freetcp.com"), "127.0.0.1"]
+server_ip = '127.0.0.1'
 server_port = 9993
-server = -1
-MSG_SIZE = 1024
 
-VLINE=chr(9474)
-username=''
+hostname, sld, tld, port = 'www', 'tecnico', 'ulisboa.pt', 80
+target = '{}.{}.{}'.format(hostname, sld, tld)
+print ('target', target)
 
-#mensagens de OK
-OK_LOC_REGIST = "{} Local registado.\n"
-OK_BALANCE = "Saldo Atual : {}.\n"
-OK_CANCEL_LOC = "1 Registo local foi cancelado.\n"
-OK_CR_ACTIV = "{} Atividade Criada.\n"
-OK_MOD_ACTIV ="1 Atividade Modificada.\n"
-OK_REM_ACTIV = "1 Atividade Removida.\n"
 
-#mensagens de erro
-ERR_REGIST_LOC_INFO = "0 Registo de Local: falta de informação.\n"
-ERR_REGIST_LOC= "0 Registo de Local: o local já existe.\n"
-ERR_CANCEL_LOC = "0 Cancelamento de Registo: o local não existe.\n"
-ERR_CR_ACTIV_TYPE = "0 Criação de atividade: atividade do mesmo tipo já existe.\n"
-ERR_CR_ACTIV_LIMIT = "0 Criação de atividade: limite maximo de atividades atingido\n"
-ERR_MOD_ACTIV_NEXIST = "0 Modificação de atividade:  atividade não existe\n"
-ERR_MOD_ACTIV_INFO = "0 Modificação de atividade: falta de informação\n"
-ERR_MOD_ACTIV_ONGOING = "0 Modificação de atividade: atividade a decorrer\n"
-ERR_REM_ACTIV_NEXIST = "0 Remoção de atividade: atividade não existente\n"
-ERR_REM_ACTIV_ONGOING =  "0 Remoção de atividade: Atividade a decorrer\n"
+
 
 def exit_sig(sig=0, frame=0):
     client.close()
     exit(0)
 
 
-
-
-while(not 0<=server<len(server_ip)):
-    server = int(input("Select server:\n0: Remote Server\n1: Localhost\n"))
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    client.connect((server_ip[server], server_port))
+    client.connect((server_ip, server_port))
 except:
-    print("ERROR: Server {} is offline".format(server_ip[server]))
+    print("ERROR: Server {} is offline".format(server_ip))
     client.close()
     exit(1)
+    
 
 signal.signal(signal.SIGINT, exit_sig)
 inputs = [client, sys.stdin]
 message=''
-print("COMMAND: ",  end='')
+print("COMMAND: ")
 sys.stdout.flush()
+    
 while True:
     ins, outs, exs = select.select(inputs, [], [])
     for i in ins:
         if(i==sys.stdin):
             response=sys.stdin.readline()
-            client.sendto(response.encode(),(server_ip[server],server_port))
+            client.sendto(response.encode(),(server_ip ,server_port))
         elif(i==client):
-            (server_msg, addr) = client.recvfrom(MSG_SIZE)
+            (server_msg, addr) = client.recvfrom(4096)
             if(not server_msg):
                 exit_sig()
             message+=str(server_msg.decode())
@@ -67,7 +46,9 @@ while True:
                 continue
             message=message.split('\n')
             for k in message:
-                process_input(k)
+                print(k)
+                if(k=="SERVER_OFF"):
+                    sys.stdout.flush()
+                    exit_sig()
             message=''
-            print("{}COMMAND: ".format(username), end='')
             sys.stdout.flush()
--- tcp-server.py
+++ tcp-server.py
@@ -2,23 +2,49 @@ import socket, sys, threading, signal, os
 from threading import RLock
 
 
-
-bind_address    = ''
-bind_port       = 9993
-server = 0
+server = -1
+bind_ip = '127.0.0.1'
+bind_port = 9993
 connections=[]
 lock = RLock()
 
 
+#mensagens de OK
+OK_LOC_REGIST = "{} Local registado.\n"
+OK_BALANCE = "Saldo Atual : {}.\n"
+OK_CANCEL_LOC = "1 Registo local foi cancelado.\n"
+OK_CR_ACTIV = "{} Atividade Criada.\n"
+OK_MOD_ACTIV ="1 Atividade Modificada.\n"
+OK_REM_ACTIV = "1 Atividade Removida.\n"
 
+#mensagens de erro
+ERR_REGIST_LOC_INFO = "0 Registo de Local: falta de informação.\n"
+ERR_REGIST_LOC= "0 Registo de Local: o local já existe.\n"
+ERR_CANCEL_LOC = "0 Cancelamento de Registo: o local não existe.\n"
+ERR_CR_ACTIV_TYPE = "0 Criação de atividade: atividade do mesmo tipo já existe.\n"
+ERR_CR_ACTIV_LIMIT = "0 Criação de atividade: limite maximo de atividades atingido\n"
+ERR_MOD_ACTIV_NEXIST = "0 Modificação de atividade:  atividade não existe\n"
+ERR_MOD_ACTIV_INFO = "0 Modificação de atividade: falta de informação\n"
+ERR_MOD_ACTIV_ONGOING = "0 Modificação de atividade: atividade a decorrer\n"
+ERR_REM_ACTIV_NEXIST = "0 Remoção de atividade: atividade não existente\n"
+ERR_REM_ACTIV_ONGOING =  "0 Remoção de atividade: Atividade a decorrer\n"
 
-def exit_server(sig, frame):
+def exit_server(sig=0, frame=0):
     print("\nExiting server recieved signal {}...".format(sig))
     server.close()
     for c in connections:
         c.send("SERVER_OFF\n".encode())
         c.close()
     exit(0)
+
+def process_input(k, client_sock):
+    k = k.split(" ")
+
+    return k
+
+
+
+
 
 
 def handle_client_connection(client_sock):
@@ -51,10 +77,10 @@ def handle_client_connection(client_sock):
 
 def main():
     global server
-    server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((bind_address, bind_port))
-    server.listen(100);
-    print("Listening on {}:{}".format(bind_address, bind_port))
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((bind_ip, bind_port))
+    server.listen(5)  # max backlog of connections
+    print("Listening on {}:{}".format(bind_ip, bind_port))
     signal.signal(signal.SIGINT, exit_server)
     while  True:
         client_sock, address = server.accept()
